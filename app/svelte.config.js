0b4635a831ddeaa1e04bda67cb5728c2ca4e35ec
HirokiYasui <hiromaily2@gmail.com>
2022-07-11 21:08:40 +0900

    update

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -25,9 +25,10 @@ const config = {
       // This can be false if you're using a fallback (i.e. SPA mode)
       default: true
     },
-		// paths: {
-		// 	base: '/svelte-sample',
-	  // },		
+		// deploy to https://hiromaily.github.io/svelte-sample/
+		paths: {
+			base: '/svelte-sample',
+	  },		
 		alias: {
 			$components: 'src/components',
 			$codec: 'src/codec'
