d12a22ca20bc26d60dbc42d0566816b32b7e0bb0
HirokiYasui <hiromaily2@gmail.com>
2022-08-14 00:25:47 +0900

    add more metamask functions

--- apps/wallet/src/components/metamask/Header.svelte
+++ apps/wallet/src/components/metamask/Header.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { Metamask, isMetamaskInstalled, openExtension } from '$lib/metamask/metamask';
+	import { chainIDMap } from '$lib/metamask/chainid';
 
 	let meta: Metamask;
 	// UI related
 	let isInstalled = false;
+	let isConnected = false;
+	let chainID = 0;
+	let networkName = '';
+	let currentAddress = '';
 
 	onMount(async () => {
 		const provider = await isMetamaskInstalled();
@@ -12,6 +17,7 @@
 			// From now on, this should always be true:
 			// provider === window.ethereum
 			console.log('provider is found');
+			isInstalled = true;
 			// create
 			meta = new Metamask(provider);
 		} else {
@@ -26,37 +32,71 @@
 	const clickConnectMetamask = async () => {
 		const account = await meta.getAccount();
 		console.log(`account: ${account}`);
-		// (async () => {
-		// 	// get account
-		// 	const accounts = await window.ethereum?.request({ method: 'eth_requestAccounts' });
-		// 	console.log(`account: ${accounts[0]}`);
-		// })();
+		isConnected = true;
+
+		// chainID (0x1)
+		const chainIDStr = await meta.chainID();
+		chainID = parseInt(chainIDStr, 16);
+
+		console.log(`chainID: ${chainID}`);
+		if (chainIDMap[chainID]) networkName = chainIDMap[chainID];
+
+		// address
+		const addr = await meta.getAddress();
+		if (addr) {
+			currentAddress = addr;
+		}
+
+		// FIXME: add network
+		meta.addEthereumChain(137);
 	};
 </script>
 
 <div class="mx-3 mt-4 row">
-	<div class="mt-2">
-		<button
-			on:click={clickInstallMetamask}
-			type="button"
-			class="btn btn-primary"
-			name="install"
-			style="width: 150px;"
-			disabled={isInstalled || null}
-		>
-			Install Metamask
-		</button>
-	</div>
-	<div class="mt-2">
-		<button
-			on:click={clickConnectMetamask}
-			type="button"
-			class="btn btn-primary"
-			name="connect"
-			style="width: 170px;"
-			disabled={!isInstalled || null}
-		>
-			Connect Metamask
-		</button>
+	<div class="mt-2" />
+
+	<div class="mx-3">
+		<div class="row">
+			<label for="inputNetwork" class="col-sm-3 col-form-label">networkVersion:</label>
+			<div class="col-sm-9">
+				<input
+					type="text"
+					class="form-control"
+					id="inputNetwork"
+					value={chainID + ' : ' + networkName}
+					readonly
+				/>
+			</div>
+			<label for="inputAddress" class="col-sm-3 col-form-label">currentAddress:</label>
+			<div class="col-sm-9">
+				<input type="text" class="form-control" id="inputAddress" value={currentAddress} readonly />
+			</div>
+
+			<div class="col-sm-9">
+				{#if isInstalled}
+					<button
+						on:click={clickConnectMetamask}
+						type="button"
+						class="btn btn-primary"
+						name="connect"
+						style="width: 170px;"
+						disabled={isConnected || null}
+					>
+						Connect Metamask
+					</button>
+				{:else}
+					<button
+						on:click={clickInstallMetamask}
+						type="button"
+						class="btn btn-primary"
+						name="install"
+						style="width: 150px;"
+						disabled={isInstalled || null}
+					>
+						Install Metamask
+					</button>
+				{/if}
+			</div>
+		</div>
 	</div>
 </div>
--- apps/wallet/src/lib/metamask/metamask.ts
+++ apps/wallet/src/lib/metamask/metamask.ts
@@ -1,5 +1,5 @@
 import detectEthereumProvider from '@metamask/detect-provider';
-
+import { chainIDParamMap } from '$lib/metamask/chainid';
 class Metamask {
 	private provider: any;
 
@@ -11,22 +11,59 @@ class Metamask {
 	// https://docs.metamask.io/guide/sending-transactions.html
 
 	public async getAccount(): Promise<string> {
+		// https://docs.metamask.io/guide/rpc-api.html#eth-requestaccounts
+		// this is equivalent to deprecated ethereum.enable()
 		const accounts = await this.provider.request({ method: 'eth_requestAccounts' });
 		return accounts[0];
 	}
 
-	public networkVersion(): number {
-		return this.provider.networkVersion; // 1
+	public async chainID(): Promise<string> {
+		//https://docs.metamask.io/guide/ethereum-provider.html#ethereum-chainid-deprecated
+		return await this.provider.request({ method: 'eth_chainId' });
 	}
 
-	public selectedAddress(): string | null {
-		return this.provider.selectedAddress; // null
+	public async networkVersion(): Promise<number> {
+		// https://docs.metamask.io/guide/ethereum-provider.html#ethereum-networkversion-deprecated
+		//return this.provider.networkVersion; // 1
+		return await this.provider.request({ method: 'net_version' });
+	}
+
+	public async getAddress(): Promise<string | null> {
+		// https://docs.metamask.io/guide/ethereum-provider.html#ethereum-selectedaddress-deprecated
+		//return this.provider.selectedAddress; // null
+		return await this.provider.request({ method: 'eth_accounts' });
+	}
+
+	public isConnected(): boolean {
+		return this.provider.isConnected;
+	}
+
+	public async addEthereumChain(chainID: number) {
+		// get chainParam
+		if (!chainIDParamMap[chainID]) {
+			console.log(`chainID: ${chainID} is not defined`);
+			return;
+		}
+
+		await this.provider.request({
+			method: 'wallet_addEthereumChain',
+			params: [chainIDParamMap[chainID]]
+		});
 	}
 
 	public readyEvent() {
+		// TODO: events
+		// https://docs.metamask.io/guide/ethereum-provider.html#events
 		this.provider.on('accountsChanged', (accounts: any) => {
 			// Time to reload your interface with accounts[0]!
-			console.log(accounts[0]);
+			console.log(`event[accountsChanged]: ${accounts[0]}`);
+		});
+		this.provider.on('chainChanged', (chainId: number) => {
+			console.log(`event[chainChanged]: ${chainId}`);
+			// Handle the new chain.
+			// Correctly handling chain changes can be complicated.
+			// We recommend reloading the page unless you have good reason not to.
+			window.location.reload();
 		});
 	}
 }
@@ -35,7 +72,12 @@ const isMetamaskInstalled = async (): Promise<unknown | undefined> => {
 	// if (typeof window.ethereum !== 'undefined' && window.ethereum.isMetaMask) {
 	//   return true;
 	// }
-	return await detectEthereumProvider();
+	const provider = await detectEthereumProvider();
+	// FIXME: how to handle??
+	if (provider !== window.ethereum) {
+		console.error('Do you have multiple wallets installed?');
+	}
+	return provider;
 };
 
 const openExtension = () => {
